68798f6ca62f990dfb2c5413dcd3358e127a8e60
ciou <yyouko22@gmail.com>
2016-12-19 13:31:59 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 
   has_many :groups
   has_many :posts
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationship, :source => :group
 end
